a7f19aa6853c0f7a8fa54aa0a634546b68f689a4
handongryong <eastdraconis@gmail.com>
2023-03-07 14:33:18 +0900

    hotfix: navigator.useragent 사용시 에러 fix
    window 생성되지않았을 때 접근하여 오류난 부분 해결

--- gatsby-ssr.tsx
+++
@@ -1,7 +0,0 @@
-import { WrapPageElementBrowserArgs } from 'gatsby';
-import React from 'react';
-import Root from './src/components/Root';
-
-export const wrapPageElement = ({ element }: WrapPageElementBrowserArgs) => {
-  return <Root>{element}</Root>;
-};
--- src/utils/mobileCheck.ts
+++ src/utils/mobileCheck.ts
@@ -1,4 +1,7 @@
 function mobileCheck() {
-  return /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
+  return (
+    typeof window !== 'undefined' &&
+    /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)
+  );
 }
 export default mobileCheck;
