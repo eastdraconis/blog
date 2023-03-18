0784bdf8b422b9321287d232a990e7f43dac1a60
handongryong <eastdraconis@gmail.com>
2023-03-18 17:19:07 +0900

    fix: CDN으로 폰트변경 후 기존에 폰트 preload하는 코드 제거

--- gatsby-ssr.tsx
+++ gatsby-ssr.tsx
@@ -2,35 +2,6 @@ import { WrapPageElementBrowserArgs } from 'gatsby';
 import React from 'react';
 import Root from './src/components/layout/Root';
 
-export const onRenderBody = ({ setHeadComponents }) => {
-  setHeadComponents([
-    <link
-      rel='preload'
-      href='/fonts/IBMPlexSansKR-Bold.ttf'
-      type='font/ttf'
-      as='font'
-      crossOrigin='anonymous'
-      key='interFont'
-    />,
-    <link
-      rel='preload'
-      href='/fonts/IBMPlexSansKR-Medium.ttf'
-      type='font/ttf'
-      as='font'
-      crossOrigin='anonymous'
-      key='interFont'
-    />,
-    <link
-      rel='preload'
-      href='/fonts/IBMPlexSansKR-Light.ttf'
-      type='font/ttf'
-      as='font'
-      crossOrigin='anonymous'
-      key='interFont'
-    />,
-  ]);
-};
-
 export const wrapPageElement = ({ element }: WrapPageElementBrowserArgs) => {
   return <Root>{element}</Root>;
 };
