d63a3922d39c0f6d2650b0fa573fd925908a1ae8
handongryong <eastdraconis@gmail.com>
2023-04-24 21:11:45 +0900

    [#23]feat: a,blockquote,strong 변환 스타일 추가

--- src/templates/PostPage.tsx
+++ src/templates/PostPage.tsx
@@ -1,4 +1,4 @@
-import { Box, Heading, Image, Text } from '@chakra-ui/react';
+import { Box, Flex, Heading, Highlight, Image, Text } from '@chakra-ui/react';
 import { MDXProvider } from '@mdx-js/react';
 import CodeBlock from 'components/CodeBlock';
 import PostLayout from 'components/layout/PostLayout';
@@ -36,11 +36,87 @@ const components = {
   h4: (props: Object) => (
     <Heading as='h4' fontSize='22px' mt='60px' pt='70px' mb='15px' {...props}></Heading>
   ),
-  p: (props: Object) => <Text fontSize='18px' {...props}></Text>,
-  img: (props: Object) => <Image {...props} width='100%' />,
+  a: (props: Object) => {
+    return (
+      <Box
+        as='a'
+        fontWeight={700}
+        _hover={{ textDecoration: 'underline' }}
+        color='blue.400'
+        target='_blank'
+        {...props}
+      />
+    );
+  },
+  blockquote: ({ ...props }) => {
+    const { children } = props;
+    return (
+      <Box
+        backgroundColor={'blue.800'}
+        _dark={{ backgroundColor: 'white' }}
+        color='black'
+        paddingLeft='5px'
+        borderRadius='10px'
+        margin='20px 0'
+        as='blockquote'
+        {...props}
+      >
+        <Flex
+          backgroundColor='blue.100'
+          _dark={{ backgroundColor: 'blue.800' }}
+          wordBreak={'keep-all'}
+          borderRadius='10px'
+          padding='20px 0'
+        >
+          <Box display='flex' alignItems='center' margin='0 10px'>
+            <svg
+              className='feather feather-help-circle'
+              fill='white'
+              height='24'
+              stroke='currentColor'
+              strokeLinecap='round'
+              strokeLinejoin='round'
+              strokeWidth='2'
+              viewBox='0 0 24 24'
+              width='24'
+              xmlns='http://www.w3.org/2000/svg'
+            >
+              <circle cx='12' cy='12' r='10' />
+              <path d='M9.09 9a3 3 0 0 1 5.83 1c0 2-3 3-3 3' />
+              <line x1='12' x2='12.01' y1='17' y2='17' />
+            </svg>
+          </Box>
+          <Box _dark={{ color: 'white' }}>{children}</Box>
+        </Flex>
+      </Box>
+    );
+  },
+  p: (props: Object) => <Text fontSize='18px' {...props} />,
+  img: (props: Object) => <Image {...props} width='100%' margin='20px 0' />,
   hr: (props: Object) => (
     <Text as='hr' mt='20px' mb='20px' {...props} borderTop='2px solid #e2e2e2e2' />
   ),
+  strong: ({ ...props }) => {
+    const { children: query } = props;
+    return (
+      <Box as='strong'>
+        <Highlight
+          query={query}
+          styles={{
+            px: '2',
+            paddingY: '0.5',
+            rounded: 'full',
+            bg: 'teal.100',
+            fontWeight: 700,
+            _dark: { color: 'white', bg: 'teal.600' },
+          }}
+          {...props}
+        >
+          {query}
+        </Highlight>
+      </Box>
+    );
+  },
   ol: (props: Object) => (
     <Box
       as='ol'
