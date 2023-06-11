547d0ba0d922c2638b9af11e4fc6ab041a010510
handongryong <eastdraconis@gmail.com>
2023-06-12 00:14:22 +0900

    [#13] feature: SEO 적용
    head api적용시 chakraUI Theme 이슈 cssVarsRoot프로퍼티로 해결

--- src/templates/AllPostTemplate.tsx
+++ src/templates/AllPostTemplate.tsx
@@ -2,7 +2,8 @@ import { Box } from '@chakra-ui/react';
 import PostCard from 'components/card/PostCard';
 import MainLayOut from 'components/layout/MainLayOut';
 import Pagenation from 'components/Pagenation';
-import { graphql } from 'gatsby';
+import { graphql, HeadFC } from 'gatsby';
+import { getSrc } from 'gatsby-plugin-image';
 
 export const query = graphql`
   query AllPostTemplate($limit: Int, $skip: Int) {
@@ -11,7 +12,11 @@ export const query = graphql`
         frontmatter {
           createdAt
           description
-          thumbnail
+          thumbnail {
+            childImageSharp {
+              gatsbyImageData
+            }
+          }
           title
           tags
           slug
@@ -22,13 +27,15 @@ export const query = graphql`
         pageCount
       }
     }
+    imageSharp(fluid: { originalName: { eq: "og-image.png" } }) {
+      gatsbyImageData
+    }
   }
 `;
 
 const AllPostTemplate = ({ data }: { data: GatsbyTypes.AllPostTemplateQuery }) => {
   const currentPage = data.allMdx.pageInfo.currentPage;
   const totalPageNumber = data.allMdx.pageInfo.pageCount;
-
   return (
     <MainLayOut>
       <Box
@@ -44,13 +51,44 @@ const AllPostTemplate = ({ data }: { data: GatsbyTypes.AllPostTemplateQuery }) =
         gap={20}
       >
         {data.allMdx.nodes.map((data, i) => {
-          return <PostCard post={data?.frontmatter!} key={i} />;
+          return (
+            <PostCard
+              slug={data.frontmatter?.slug!}
+              key={i}
+              title={data.frontmatter?.title!}
+              description={data.frontmatter?.description!}
+              createdAt={data.frontmatter?.createdAt!}
+              tags={data.frontmatter?.tags!}
+              thumbnail={data.frontmatter?.thumbnail?.childImageSharp?.gatsbyImageData!}
+            />
+          );
         })}
       </Box>
       {totalPageNumber > 1 && (
         <Pagenation currentPage={currentPage} totalPageNumber={totalPageNumber} />
       )}
     </MainLayOut>
+  );
+};
+
+export const Head: HeadFC<GatsbyTypes.AllPostTemplateQuery> = ({ data }) => {
+  const ogImage = data.imageSharp?.gatsbyImageData!;
+  const title = '한동룡의 기술 블로그';
+  const description = '주니어 프론트엔드 개발자 한동룡 입니다';
+
+  return (
+    <>
+      <title>{title}</title>
+      <meta name='description' content={description}></meta>
+      <meta name='viewport' content='width=device-width, initial-scale=1' />
+      <meta name='naver-site-verification' content='4fc69b1ccb5d3fe134d2663f45be860476f4d8ef' />
+      <meta property='og:url' content='https://handongryong.com' />
+      <meta property='og:type' content='website' />
+      <meta property='og:site_name' content={title} />
+      <meta property='og:title' content={title} />
+      <meta property='og:description' content={description} />
+      <meta property='og:image' content={getSrc(ogImage)} />
+    </>
   );
 };
 export default AllPostTemplate;
--- src/templates/PostPage.tsx
+++ src/templates/PostPage.tsx
@@ -1,9 +1,10 @@
-import { Box, Image } from '@chakra-ui/react';
+import { Box, Divider, Heading, Text } from '@chakra-ui/react';
 import Giscus from 'components/Giscus';
 import PostLayout from 'components/layout/PostLayout';
 import MdxConvert from 'components/MdxConvert';
 import Toc from 'components/Toc';
-import { graphql } from 'gatsby';
+import { graphql, HeadFC } from 'gatsby';
+import { GatsbyImage, getSrc } from 'gatsby-plugin-image';
 
 export const query = graphql`
   query Post($id: String) {
@@ -14,22 +15,48 @@ export const query = graphql`
         description
         slug
         tags
-        thumbnail
         title
         updatedAt
+        thumbnail {
+          childImageSharp {
+            gatsbyImageData
+          }
+        }
       }
       tableOfContents
     }
   }
 `;
-
-const PostPage = ({ data, children }: { data: any; children: React.ReactNode }) => {
-  const thumbnail = data.mdx.frontmatter.thumbnail;
-  const tableOfContents = data.mdx.tableOfContents;
+interface PostPageProps {
+  data: GatsbyTypes.PostQuery;
+  children: React.ReactNode;
+}
+const PostPage = ({ data, children }: PostPageProps) => {
+  const tableOfContents = data.mdx?.tableOfContents;
   return (
     <PostLayout>
       <Box as='article' width='100%' minWidth={{ sm: '0', xl: '700px' }} paddingBottom='300px'>
-        <Image src={thumbnail} htmlWidth='100%' borderRadius='10px' marginBottom='100px' />
+        <Box marginTop='50px'>
+          <GatsbyImage
+            style={{
+              borderRadius: '20px',
+              width: '100%',
+              maxHeight: '400px',
+              objectFit: 'cover',
+            }}
+            alt='thumbnail'
+            image={data.mdx?.frontmatter?.thumbnail?.childImageSharp?.gatsbyImageData!}
+          />
+        </Box>
+        <Heading marginTop='60px' fontWeight='700' fontSize='48px' marginBottom='10px'>
+          {data.mdx?.frontmatter?.title}
+        </Heading>
+        <Box marginBottom='50px'>
+          <Text width='auto' marginBottom='20px'>
+            {data.mdx?.frontmatter?.createdAt}
+          </Text>
+          <Divider borderBottomWidth='3px' borderColor='blackAlpha.400' borderRadius='20px' />
+        </Box>
         <MdxConvert mdxContent={children} />
         <Giscus />
       </Box>
@@ -37,6 +64,29 @@ const PostPage = ({ data, children }: { data: any; children: React.ReactNode }) 
         <Toc tableOfContents={tableOfContents} />
       </Box>
     </PostLayout>
+  );
+};
+
+export const Head: HeadFC<GatsbyTypes.PostQuery> = ({ data }) => {
+  const title = data.mdx?.frontmatter?.title!;
+  const description = data.mdx?.frontmatter?.description!;
+  const ogImage = data.mdx?.frontmatter?.thumbnail?.childImageSharp?.gatsbyImageData!;
+  return (
+    <>
+      <title>{title}</title>
+      <meta name='description' content={description} />
+      <meta name='naver-site-verification' content='4fc69b1ccb5d3fe134d2663f45be860476f4d8ef' />
+
+      <meta
+        property='og:url'
+        content={`https://handongryong.com/post/${data.mdx?.frontmatter?.slug}`}
+      />
+      <meta property='og:type' content='website' />
+      <meta property='og:site_name' content={title} />
+      <meta property='og:title' content={title} />
+      <meta property='og:description' content={description} />
+      <meta property='og:image' content={getSrc(ogImage)} />
+    </>
   );
 };
 export default PostPage;
