13d069956e738198d98677fde2f1040263130c0d
handongryong <eastdraconis@gmail.com>
2023-06-12 00:14:55 +0900

    [#13] feature: cssvarsRoot적용

--- src/components/layout/Root.tsx
+++ src/components/layout/Root.tsx
@@ -2,7 +2,11 @@ import { ChakraProvider } from '@chakra-ui/react';
 import theme from 'components/chakra/theme';
 
 function Root({ children }: { children: React.ReactNode }) {
-  return <ChakraProvider theme={theme}>{children}</ChakraProvider>;
+  return (
+    <ChakraProvider cssVarsRoot='body' theme={theme}>
+      {children}
+    </ChakraProvider>
+  );
 }
 
 export default Root;
