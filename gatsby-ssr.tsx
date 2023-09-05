cbfeaab51a07cf1fb7073ceab5ae1ddc304f9b87
handongryong <eastdraconis@gmail.com>
2023-09-06 00:08:54 +0900

    [#32] feature: html lang ko 설정

--- gatsby-ssr.tsx
+++ gatsby-ssr.tsx
@@ -5,3 +5,8 @@ import Root from './src/components/layout/Root';
 export const wrapPageElement = ({ element }: WrapPageElementBrowserArgs) => {
   return <Root>{element}</Root>;
 };
+
+export const onRenderBody = (gatsbyUtils) => {
+  const { setHtmlAttributes } = gatsbyUtils;
+  setHtmlAttributes({ lang: 'ko' });
+};
