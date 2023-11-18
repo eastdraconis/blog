ed95bc7b1f8342619a415a20d360039b9cb37ea7
handongryong <eastdraconis@gmail.com>
2023-11-19 00:43:40 +0900

    feature: logo, resume hover이벤트 적용

--- src/components/Header/About.tsx
+++ src/components/Header/About.tsx
@@ -2,9 +2,41 @@ import { Text } from '@chakra-ui/react';
 
 const About = () => {
   return (
-    <a href='https://handongryong.super.site/' target='_blank' rel='noreferrer'>
-      <Text fontSize={16} fontStyle='italic' padding={2} fontWeight={700}>
-        About
+    <a
+      href='https://www.figma.com/file/F6FL05hGZ2Dqkl8B8NMEhC/%ED%95%9C%EB%8F%99%EB%A3%A1-%EC%9D%B4%EB%A0%A5%EC%84%9C?type=design&node-id=0%3A1&mode=design&t=juTDnEH5UqRPqOzf-1'
+      target='_blank'
+      rel='noreferrer'
+    >
+      <Text
+        position='relative'
+        _after={{
+          position: 'absolute',
+          content: '""',
+          height: '1px',
+          bottom: '-1px',
+          left: '0',
+          width: '100%',
+          bg: 'black',
+          transition: 'transform 0.2s ease-out',
+          transformOrigin: 'bottom right',
+          transform: 'scaleX(0)',
+        }}
+        fontSize={16}
+        fontStyle='italic'
+        fontWeight={'bold'}
+        _dark={{
+          _after: {
+            bg: 'white',
+          },
+        }}
+        _hover={{
+          _after: {
+            transform: 'scaleX(1)',
+            transformOrigin: 'bottom left',
+          },
+        }}
+      >
+        Resume
       </Text>
     </a>
   );
--- src/components/Header/Logo.tsx
+++ src/components/Header/Logo.tsx
@@ -4,7 +4,35 @@ import { Link } from 'gatsby';
 const Logo = () => {
   return (
     <Link to='/'>
-      <Text fontSize={24} fontWeight='700' padding={2} fontStyle='italic'>
+      <Text
+        position='relative'
+        _after={{
+          position: 'absolute',
+          content: '""',
+          height: '1px',
+          bottom: '-1px',
+          left: '0',
+          width: '100%',
+          bg: 'black',
+          transition: 'transform 0.2s ease-out',
+          transformOrigin: 'bottom right',
+          transform: 'scaleX(0)',
+        }}
+        fontSize={24}
+        fontWeight='bold'
+        fontStyle={'italic'}
+        _dark={{
+          _after: {
+            bg: 'white',
+          },
+        }}
+        _hover={{
+          _after: {
+            transform: 'scaleX(1)',
+            transformOrigin: 'bottom left',
+          },
+        }}
+      >
         Ryong⥾
       </Text>
     </Link>
