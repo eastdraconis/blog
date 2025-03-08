e0d23a41c6bef02bfc8b9bfa50bf453e0b76b6b9
Drake <eastdraconis@gmail.com>
2025-03-08 20:12:42 +0900

    Revert "chore: Webpack 설정에 절대 경로 별칭 추가"
    This reverts commit b4903eec3d53c57455b5e40259acbac2ff2869b6.

--- next.config.ts
+++ next.config.ts
@@ -1,20 +1,9 @@
 import type { NextConfig } from 'next';
 import { createVanillaExtractPlugin } from '@vanilla-extract/next-plugin';
-import path from 'path';
 const withVanillaExtract = createVanillaExtractPlugin();
 
 const nextConfig: NextConfig = {
   transpilePackages: ['next-mdx-remote'],
-  webpack: (config) => {
-    config.resolve = {
-      alias: {
-        '@/': path.resolve(__dirname, 'src'),
-      },
-      ...config.resolve,
-    };
-
-    return config;
-  },
   images: {
     remotePatterns: [
       {
