b4903eec3d53c57455b5e40259acbac2ff2869b6
Drake <eastdraconis@gmail.com>
2025-03-08 20:07:23 +0900

    chore: Webpack 설정에 절대 경로 별칭 추가

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,20 @@
 import type { NextConfig } from 'next';
 import { createVanillaExtractPlugin } from '@vanilla-extract/next-plugin';
+import path from 'path';
 const withVanillaExtract = createVanillaExtractPlugin();
 
 const nextConfig: NextConfig = {
   transpilePackages: ['next-mdx-remote'],
+  webpack: (config) => {
+    config.resolve = {
+      alias: {
+        '@/': path.resolve(__dirname, 'src'),
+      },
+      ...config.resolve,
+    };
+
+    return config;
+  },
   images: {
     remotePatterns: [
       {
